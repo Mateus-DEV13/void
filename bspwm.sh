88084c6de8607ff3899606110c39ed7c9a0ee3d3
Mateus Mota Bomfim <61365094+Silver-Devil@users.noreply.github.com>
2021-06-17 15:50:11 -0300

    Create bspwm.sh

---
+++ bspwm.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mkdir ~/.config && mkdir -p ~/.config/{bspwm,sxhkd} && cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/ && cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/ && chmod u+x ~/.config/bspwm/bspwmrc
