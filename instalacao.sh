d0f8621e146f83481c7fc840bd439a063a04dc96
Mateus Mota Bomfim <61365094+Silver-Devil@users.noreply.github.com>
2021-06-17 15:44:03 -0300

    Create instalacao.sh

---
+++ instalacao.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo xbps-install -Syu && sudo xbps-install feh bspwm NetworkManager Thunar thunar-archive-plugin xfce4-screenshooter vim firefox gimp blueman eclipse gucharmap transmission-gtk vlc bluez alsa-lib telegram-desktop qemu libvirt polybar st
