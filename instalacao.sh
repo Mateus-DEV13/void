da9426b203303636b4227ea65ddb1d3ae4188803
Mateus Mota Bomfim <61365094+Silver-Devil@users.noreply.github.com>
2021-06-17 16:05:39 -0300

    Update instalacao.sh

--- instalacao.sh
+++ instalacao.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo xbps-install -Syu && sudo xbps-install feh bspwm NetworkManager dunst lxappearance network-manager-applet Thunar thunar-archive-plugin xfce4-screenshooter vim firefox gimp blueman eclipse gucharmap transmission-gtk vlc bluez alsa-lib telegram-desktop qemu libvirt polybar st
+sudo xbps-install -Syu && sudo xbps-install feh bspwm NetworkManager dunst lxappearance network-manager-applet Thunar thunar-archive-plugin xfce4-screenshooter vim firefox gimp blueman eclipse gucharmap transmission-gtk vlc bluez alsa-lib telegram-desktop qemu libvirt polybar st python3-pip
